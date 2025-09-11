a4c86360a1f119d606d8448e41885ee28b5410c7
josephhieu <joseph.nguyenhoanghieu@gmail.com>
2025-09-11 20:52:12 +0700

    React Router - Switch and Redirect Routing

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -4,21 +4,26 @@ import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
 import { HomePage } from "./layouts/HomePage/HomePage";
 import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
-import { Route } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 
 export const App = () => {
   return (
     <div>
       <Navbar />
 
-      <Route path="/">
-        <HomePage />
-      </Route>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/home" />
+        </Route>
 
-      <Route path="/search">
-        <SearchBooksPage />
-      </Route>
+        <Route path="/home">
+          <HomePage />
+        </Route>
 
+        <Route path="/search">
+          <SearchBooksPage />
+        </Route>
+      </Switch>
       <Footer />
     </div>
   );
