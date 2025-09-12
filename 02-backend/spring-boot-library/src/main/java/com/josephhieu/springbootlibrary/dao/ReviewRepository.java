1ec0d0c56a36ad3a20a55959af7b0865e3d7270c
josephhieu <joseph.nguyenhoanghieu@gmail.com>
2025-09-12 11:01:40 +0700

    Spring boot - Find Review by ID

--- 02-backend/spring-boot-library/src/main/java/com/josephhieu/springbootlibrary/dao/ReviewRepository.java
+++ 02-backend/spring-boot-library/src/main/java/com/josephhieu/springbootlibrary/dao/ReviewRepository.java
@@ -1,7 +1,13 @@
 package com.josephhieu.springbootlibrary.dao;
 
 import com.josephhieu.springbootlibrary.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Page<Review> findByBookId(@RequestParam("book_id") Long bookId,
+                              Pageable pageable);
 }
