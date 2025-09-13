f49f687125af93de3c84102dd6876f5921eb370a
josephhieu <joseph.nguyenhoanghieu@gmail.com>
2025-09-13 16:29:21 +0700

    Create/Update auth0Config.ts

---
+++ 03-frontend/react-library/src/lib/auth0Config.ts
@@ -0,0 +1,7 @@
+export const auth0Config = {
+  clientId: "dxVQWvMGbUr464yGFYanEwsYquOzclt5",
+  issuer: "dev-f7oen7czmims7xcv.us.auth0.com",
+  audience: "http://localhost:8080",
+  redirectUri: window.location.origin + "/callback",
+  scope: "openid profile email",
+};
