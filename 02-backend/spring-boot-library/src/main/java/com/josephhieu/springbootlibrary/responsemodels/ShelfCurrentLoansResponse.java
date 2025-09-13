566327816dae9ca75f5e8cee2ad482663076641b
josephhieu <joseph.nguyenhoanghieu@gmail.com>
2025-09-13 20:17:15 +0700

    Spring Boot - add ShelfCurrentLoansResponse.java in responsemodels folders

---
+++ 02-backend/spring-boot-library/src/main/java/com/josephhieu/springbootlibrary/responsemodels/ShelfCurrentLoansResponse.java
@@ -0,0 +1,17 @@
+package com.josephhieu.springbootlibrary.responsemodels;
+
+import com.josephhieu.springbootlibrary.entity.Book;
+import lombok.Data;
+
+@Data
+public class ShelfCurrentLoansResponse {
+
+    public ShelfCurrentLoansResponse(Book book, int daysLeft) {
+        this.book = book;
+        this.daysLeft = daysLeft;
+    }
+
+    private Book book;
+
+    private int daysLeft;
+}
